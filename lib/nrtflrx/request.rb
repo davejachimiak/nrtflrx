a9bd044509b979a5fa55add354bf155ff10abe70
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-10-04 21:44:33 -0400

    Initialize and implement resource path on Request

--- lib/nrtflrx/request.rb
+++ lib/nrtflrx/request.rb
@@ -1,5 +1,11 @@
 module Nrtflrx
   class Request
+    attr_reader :resource_path
+
+    def initialize(resource_path)
+      @resource_path = resource_path
+    end
+
     def execute
     end
   end
--- spec/nrtflrx/request_spec.rb
+++ spec/nrtflrx/request_spec.rb
@@ -4,4 +4,31 @@ require 'minitest/autorun'
 require 'mocha'
 
 describe Nrtflrx::Request do
+  before do
+    @request = Nrtflrx::Request.new('clown sauce')
+  end
+
+  describe '#initialize' do
+    it 'instantiates the resource_path with poop' do
+      resource_path_ivar = @request.instance_variable_get(:@resource_path)
+
+      resource_path_ivar.must_equal 'clown sauce'
+    end
+
+    it 'instantiates the resource_path with great' do
+      request            = Nrtflrx::Request.new('great')
+      resource_path_ivar = request.instance_variable_get(:@resource_path)
+
+      resource_path_ivar.must_equal 'great'
+    end
+  end
+
+  describe '#resource_path' do
+    it 'is an attr_reader' do
+      request = Nrtflrx::Request.new('clown sauce')
+      resource_path = @request.resource_path
+
+      resource_path.must_equal 'clown sauce'
+    end
+  end
 end
