96429a377986309d20fdc9d95e1de64b72f45c27
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-10-07 13:50:28 -0400

    Refactor Params#get

--- lib/nrtflrx/request/params.rb
+++ lib/nrtflrx/request/params.rb
@@ -2,13 +2,21 @@ module Nrtflrx
   class Request
     class Params
       def get
-        method_names = self.public_methods(false).select do |methods|
-          methods != __method__
-        end
+        public_methods = self.public_methods false
+        param_names    = public_methods.reject { |method| method == __method__ }
 
-        Hash[method_names.map do |method_name|
-          [method_name, self.send(method_name)]
-        end]
+        Hash[params_with_values(param_names)]
+      end
+
+    private
+      def params_with_values(param_names)
+        param_names.map do |param_name|
+          [param_name, param_value(param_name)]
+        end
+      end
+
+      def param_value(param_name)
+        self.send(param_name)
       end
     end
   end
