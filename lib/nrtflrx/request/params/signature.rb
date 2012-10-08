54abad5bceacaa7843bde9cc8fb8f913b438c223
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-10-08 17:47:15 -0400

    Implement Signature#sign

--- lib/nrtflrx/request/params/signature.rb
+++ lib/nrtflrx/request/params/signature.rb
@@ -1,8 +1,10 @@
+require 'cgi'
+
 module Nrtflrx
   class Request
     class Params
       class Signature
-        attr_reader :request
+        attr_reader :params
 
         def initialize(request, params)
           @request = request
@@ -10,6 +12,14 @@ module Nrtflrx
         end
 
         def sign
+          sha1               = OpenSSL::Digest::SHA1.new
+          oauth_consumer_key = params.oauth_consumer_key
+
+          raw_signature             = OpenSSL::HMAC.digest(sha1, oauth_consumer_key, base_string)
+          encoded_signature         = Base64.encode64(raw_signature)
+          chomped_encoded_signature = encoded_signature.chomp
+
+          CGI.escape(chomped_encoded_signature)
         end
       end
     end
--- spec/nrtflrx/request/params/signature_spec.rb
+++ spec/nrtflrx/request/params/signature_spec.rb
@@ -5,11 +5,10 @@ require 'mocha'
 require 'openssl'
 require 'base64'
 
-
 describe Nrtflrx::Request::Params::Signature do
   before do
-    @request_mock = mock
-    @signature = Nrtflrx::Request::Params::Signature.new(@request_mock, 'parums')
+    @params_mock = mock
+    @signature   = Nrtflrx::Request::Params::Signature.new('please', @params_mock)
   end
 
   describe 'initialize' do
@@ -17,7 +16,7 @@ describe Nrtflrx::Request::Params::Signature do
       it 'one way' do
         request_ivar = @signature.instance_variable_get :@request
 
-        request_ivar.must_equal @request_mock
+        request_ivar.must_equal 'please'
       end
 
       it 'another way' do
@@ -32,7 +31,7 @@ describe Nrtflrx::Request::Params::Signature do
       it 'one way' do
         request_ivar = @signature.instance_variable_get :@params
 
-        request_ivar.must_equal 'parums'
+        request_ivar.must_equal @params_mock
       end
 
       it 'another way' do
@@ -44,11 +43,11 @@ describe Nrtflrx::Request::Params::Signature do
     end
   end
 
-  describe '#request' do
+  describe '#params' do
     it 'returns the request ivar' do
-      request = @signature.request
+      params = @signature.params
 
-      request.must_equal @request_mock
+      params.must_equal @params_mock
     end
   end
 
@@ -56,17 +55,18 @@ describe Nrtflrx::Request::Params::Signature do
     it 'uses a SHA1 object to sign base string with consumer key, encodes ' +
       'the signature with Base64, chomps the encoding, and escapes the ' +
       'signature with CGI' do
-      skip 'TODO: consumer key as attr_reader'
-      request = mock(:oauth_consumer_key)
-      request.stubs(:oauth_consumer_key).returns 'consumer key'
-      signature = Nrtflrx::Request::Params::Signature.new(request, 'params')
+      params = Nrtflrx::Request::Params.new('request')
+      params.stubs(:oauth_consumer_key).returns 'consumer key'
+      Nrtflrx::Request::Params.stubs(:new).with('request').returns params
+
+      signature = Nrtflrx::Request::Params::Signature.new('request', params)
       signature.stubs(:base_string).returns 'base string'
       OpenSSL::Digest::SHA1.stubs(:new).returns 'the digest'
       OpenSSL::HMAC.stubs(:digest).with('the digest', 'consumer key', 'base string').
         returns 'raw signature'
-      Base64.stubs(:encode64).with('raw signature').returns 'encoded signature=\n'
+      Base64.stubs(:encode64).with('raw signature').returns "encoded signature=\n"
 
-      signed_signature = @signature.sign
+      signed_signature = signature.sign
 
       signed_signature.must_equal 'encoded+signature%3D'
     end
