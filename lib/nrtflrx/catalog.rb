266a4c7e4d7c2f89f6de79a0bc7511ccefce7548
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-10-14 23:13:49 -0400

    Fix catalog path

--- lib/nrtflrx/catalog.rb
+++ lib/nrtflrx/catalog.rb
@@ -3,7 +3,7 @@ require_relative 'request'
 module Nrtflrx
   class Catalog
     def self.request
-      request = Nrtflrx::Request.new('catalog/titles')
+      request = Nrtflrx::Request.new('catalog/titles/index')
 
       request.execute
     end
--- spec/nrtflrx/catalog_spec.rb
+++ spec/nrtflrx/catalog_spec.rb
@@ -8,7 +8,7 @@ describe Nrtflrx::Catalog do
     it 'executes a request with the catalog path' do
       request = Nrtflrx::Request.new('catalog/titles')
       request.expects(:execute)
-      Nrtflrx::Request.expects(:new).with('catalog/titles').returns request
+      Nrtflrx::Request.expects(:new).with('catalog/titles/index').returns request
 
       Nrtflrx::Catalog.request
     end
