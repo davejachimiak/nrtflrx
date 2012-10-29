03900c176124c82b794238bad3c32d5854cc4808
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-10-28 22:37:19 -0400

    Fix catalog spec

--- spec/nrtflrx/catalog_spec.rb
+++ spec/nrtflrx/catalog_spec.rb
@@ -4,7 +4,7 @@ require_relative '../spec_helper'
 describe Nrtflrx::Catalog do
   describe '#request' do
     it 'executes a request with the catalog path' do
-      request = Nrtflrx::Request.new('catalog/titles')
+      request = Nrtflrx::Request.new('catalog/titles/index')
       request.expects(:execute)
       Nrtflrx::Request.expects(:new).with('catalog/titles/index').returns request
 
