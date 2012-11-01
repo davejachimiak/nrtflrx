41654963202c52966f70a43635e33df98d0d3dfd
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-11-01 08:48:07 -0400

    Begin implementing user authenticators

--- lib/nrtflrx/user_authenticators.rb
+++ lib/nrtflrx/user_authenticators.rb
@@ -1,4 +1,19 @@
 module Nrtflrx
   class UserAuthenticators
+    def initialize(login_info)
+      @name     = login_info[:name]
+      @password = login_info[:password]
+
+      get_request_token
+      set_authenticators
+    end
+
+    def get_request_token
+      request_token_request = Nrtflrx::Request.new('oauth/request_token')
+      @request_token        = request_token_request.execute
+    end
+
+    def set_authenticators
+    end
   end
 end
---
+++ spec/nrtflrx/user_authenticators_spec.rb
@@ -0,0 +1,52 @@
+require_relative '../spec_helper'
+require_relative '../../lib/nrtflrx'
+
+require 'net/http'
+
+describe Nrtflrx::UserAuthenticators do
+  describe 'initialize' do
+    before do
+      @login_info          = { name: 'cool@cool.com', password: 'cool' }
+      @user_authenticators = Nrtflrx::UserAuthenticators.new(@login_info)
+    end
+
+    it 'initializes @name' do
+      name_ivar = @user_authenticators.instance_variable_get :@name
+
+      name_ivar.must_equal 'cool@cool.com'
+    end
+
+    it 'initializes @password' do
+      password_ivar = @user_authenticators.instance_variable_get :@password
+
+      password_ivar.must_equal 'cool'
+    end
+
+    it 'calls get_request_token' do
+      Nrtflrx::UserAuthenticators.any_instance.expects(:get_request_token)
+
+      Nrtflrx::UserAuthenticators.new(@login_info)
+    end
+
+    it 'calls set_authenticators' do
+      Nrtflrx::UserAuthenticators.any_instance.expects(:set_authenticators)
+
+      Nrtflrx::UserAuthenticators.new(@login_info)
+    end
+  end
+
+  describe '#get_request_token' do
+    it 'gets the request token and sets it to @request_token' do
+      request = Nrtflrx::Request.new('oauth/request_token')
+      request.stubs(:execute).returns 'Eyes Wide Shut'
+      Nrtflrx::Request.stubs(:new).with('oauth/request_token').returns request
+
+      request_token_ivar = @user_authenticators.instance_variable_get :@request_token
+
+      request_token_ivar.must_equal 'Eyes Wide Shut'
+    end
+  end
+
+  describe '#set_authenticators' do
+  end
+end
