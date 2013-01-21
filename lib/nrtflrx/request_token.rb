2c1295554ad2cdc40232c51f52f67b4ac09d5f7c
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-01-21 14:35:46 -0500

    Automatically methodize Nrtflrx::RequestToken

--- lib/nrtflrx/request_token.rb
+++ lib/nrtflrx/request_token.rb
@@ -1,7 +1,18 @@
-class Nrtflrx::RequestToken
-  attr_reader :as_params_string
+require 'ostruct'
 
+class Nrtflrx::RequestToken
   def initialize
-    @as_params_string = Nrtflrx::Request.new('oauth/request_token').submit
+    response_string = Nrtflrx::Request.new('oauth/request_token').submit
+    response_hash   = Hash[CGI.parse(response_string).map do |k, v|
+      [k, v.first]
+    end]
+
+    self.class.class_eval do
+      response_hash.each_pair do |k, v|
+        define_method k.to_sym do
+          v
+        end
+      end
+    end
   end
 end
--- spec/nrtflrx/request_token_spec.rb
+++ spec/nrtflrx/request_token_spec.rb
@@ -2,15 +2,24 @@ require_relative '../spec_helper'
 require_relative '../../lib/nrtflrx'
 
 describe Nrtflrx::RequestToken do
-  describe '#initialize' do
-    it 'sets the bare request token to #as_params_string' do
-      request = Nrtflrx::Request.new 'oauth/request_token'
-      Nrtflrx::Request.stubs(:new).with('oauth/request_token').returns request
-      request.stubs(:submit).returns 'PARAMS STRING'
+  before do
+    request = Nrtflrx::Request.new 'oauth/request_token'
+    Nrtflrx::Request.stubs(:new).with('oauth/request_token').returns request
+    request.stubs(:submit).
+      returns 'oauth_token=psvd6ta236taungqxrjhbkat&oauth_token_secret=ejSFZuWhe7eE&application_name=A+Better+Queue&login_url=https%3A%2F%2Fapi-user.netflix.com%2Foauth%2Flogin%3Foauth_token%3Dpsvd6ta236taungqxrjhbkat'
 
-      request_token = Nrtflrx::RequestToken.new
+    @request_token = Nrtflrx::RequestToken.new
+  end
 
-      request_token.as_params_string.must_equal 'PARAMS STRING'
+  describe '#oauth_token' do
+    it 'returns the oauth token' do
+      @request_token.oauth_token.must_equal 'psvd6ta236taungqxrjhbkat'
+    end
+  end
+
+  describe '#oauth_token_secret' do
+    it 'returns the oauth token secret' do
+      @request_token.oauth_token_secret.must_equal 'ejSFZuWhe7eE'
     end
   end
 end
