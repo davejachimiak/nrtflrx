4049d6374a7f2d756e891e858820adbaf4f09a03
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-01-20 20:09:07 -0500

    Make specs work with turn

--- spec/nrtflrx_spec.rb
+++ spec/nrtflrx_spec.rb
@@ -4,8 +4,6 @@ require_relative 'spec_helper'
 describe Nrtflrx do
   describe '#consumer_key' do
     it 'is an attr_accessor' do
-      Nrtflrx.consumer_key.must_be_nil
-
       Nrtflrx.consumer_key = 'BLARGH'
 
       Nrtflrx.consumer_key.must_equal 'BLARGH'
@@ -14,8 +12,6 @@ describe Nrtflrx do
 
   describe '#shared_secret' do
     it 'is an attr_accessor' do
-      Nrtflrx.shared_secret.must_be_nil
-
       Nrtflrx.shared_secret = 'BLURGH'
 
       Nrtflrx.shared_secret.must_equal 'BLURGH'
