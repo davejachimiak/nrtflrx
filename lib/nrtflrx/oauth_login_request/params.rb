c99479ee616ccf66d86845bd25456dc37cd6d045
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-02-02 17:20:33 -0500

    Initialize OAuthLoginRequest::Params

--- lib/nrtflrx/oauth_login_request/params.rb
+++ lib/nrtflrx/oauth_login_request/params.rb
@@ -1,6 +1,14 @@
 module Nrtflrx
   class OAuthLoginRequest
     class Params
+      def initialize email, password, request_token
+        @name               = email
+        @password           = password
+        @oauth_token        = request_token.oauth_token
+        @application_name   = request_token.application_name
+        @accept_tos         = true
+        @oauth_consumer_key = Nrtflrx.consumer_key
+      end
     end
   end
 end
--- spec/nrtflrx/oauth_login_request/params_spec.rb
+++ spec/nrtflrx/oauth_login_request/params_spec.rb
@@ -3,7 +3,42 @@ require_relative '../../../lib/nrtflrx'
 
 describe Nrtflrx::OAuthLoginRequest::Params do
   describe '#initialize' do
-    it 'sets accept_tos to true'
-    it 'sets consumer key'
+    before do
+      Nrtflrx.consumer_key = @consumer_key = 'consumer key'
+      @email    = 'email'
+      @password = 'password'
+      request_token = OpenStruct.new(
+        oauth_token:      @oauth_token      = 'ot',
+        application_name: @application_name = 'an'
+      )
+      @params   = Nrtflrx::OAuthLoginRequest::Params.
+        new @email, @password, request_token
+    end
+
+    it 'sets name to the email passed in' do
+      @params.instance_variable_get(:@name).must_equal @email
+    end
+
+    it 'sets password to the password passed in' do
+      @params.instance_variable_get(:@password).must_equal @password
+    end
+
+    it 'sets the oauth token from the request token passed in' do
+      @params.instance_variable_get(:@oauth_token).must_equal @oauth_token
+    end
+
+    it 'sets the application name from the request token passed in' do
+      @params.instance_variable_get(:@application_name).
+        must_equal @application_name
+    end
+
+    it 'sets accept_tos to true' do
+      @params.instance_variable_get(:@accept_tos).must_equal true
+    end
+
+    it 'sets consumer key' do
+      @params.instance_variable_get(:@oauth_consumer_key).
+        must_equal @consumer_key
+    end
   end
 end
