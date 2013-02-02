3010c544eebdc999ee1a6780f6b4c37b18eb5e75
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-02-02 09:33:58 -0500

    Initialize oauth login request

--- spec/nrtflrx/oauth_login_request_spec.rb
+++ spec/nrtflrx/oauth_login_request_spec.rb
@@ -3,12 +3,18 @@ require_relative '../../lib/nrtflrx'
 
 describe Nrtflrx::OAuthLoginRequest do
   describe 'initialize' do
+    before do
+      @email = 'email'
+      @password = 'password'
+      @oauth_login_request = Nrtflrx::OAuthLoginRequest.new @email, @password
+    end
+
     it 'sets the email passed in' do
-      email = 'email'
+      @oauth_login_request.email.must_equal @email
+    end
 
-      oauth_login_request = Nrtflrx::OAuthLoginRequest.new email
-
-      oauth_login_request.email.must_equal email
+    it 'sets the password passed in' do
+      @oauth_login_request.password.must_equal @password
     end
   end
 end
